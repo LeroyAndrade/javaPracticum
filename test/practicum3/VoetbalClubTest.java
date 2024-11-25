f6f23b52187304dfb7c1a7eb608897815ba83518
LeroyAndrade <leroy_andrade@hotmail.com>
2024-11-25 19:58:49 +0100

    + Tussentijdse TestOpslag

--- test/practicum3/VoetbalClubTest.java
+++ test/practicum3/VoetbalClubTest.java
@@ -3,6 +3,7 @@ package practicum3;
 import inlesTheorie.Rekening;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import practicum2.practicumB.Voetbalclub;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,13 +11,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class VoetbalClubTest {
 
-    //    init is een methode die altijd wordt gerunt voordat de rest wordt gerunt
-    // zo doe je dus Dont repeat yourself
-    private Rekening r;
+    private Voetbalclub club;
     @BeforeEach
     public void init(){
         System.out.println("init");
-        r = new Rekening(13245698);
+        club = new Voetbalclub("Fejj");
+        //    init is een methode die altijd wordt gerunt voordat de rest wordt gerunt
+        // zo doe je dus Dont repeat yourself
+        Rekening r = new Rekening(13245698);
+    }
+
+
+    @Test
+    public void clubNaam_null_or_empty_ThenSetClubName_FC() {
+         // Creating a club with null name
+//        club.voetbalclubNaam = "FC ";
+//        club.voetbalclubNaam = "";
+//        club.voetbalclubNaam = null;
+        if (club.voetbalclubNaam.isEmpty() || this.club.voetbalclubNaam == null) {
+            club.voetbalclubNaam = "FC";
+        }
+        assertEquals("FC", club.voetbalclubNaam, "Club name should default to 'FC' if null or empty.");
     }
 
     @Test
@@ -29,6 +44,8 @@ public class VoetbalClubTest {
 //        assertEquals(100.000000000000007, r.getSaldo()); //slaagt ook in de test
 
     }
+
+
 
 //    @Test
 //    void tes_leeftijdGroterDan76EnInBezitMuseumpas_Kortingtoekennen(){}

65896b23191f689a90d6554d2a03831fa2e6f770
LeroyAndrade <leroy_andrade@hotmail.com>
2024-11-28 03:05:06 +0100

    + Code werkt nu met eigen exception.

--- src/practicum3/practicum3B/Cirkel.java
+++ src/practicum3/practicum3B/Cirkel.java
@@ -11,8 +11,9 @@ public class Cirkel {
         this.yPositie = yPositie;
 
             if (radius <=0) {
-                throw new IllegalArgumentException ("Radius moet groter dan 0 zijn!");
+                throw new LeroyException ("Radius moet groter dan 0 zijn!");
             }
+
     }
 
     public void setRadius(int radius) {
@@ -25,18 +26,17 @@ public class Cirkel {
                 +"met radius: " + radius;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LeroyException {
         Cirkel c1 = null, c2 = null;
 
         try {
             c1 = new Cirkel(10, 0, 0);
             c2 = new Cirkel(0, 10, 10);
-        } catch (IllegalArgumentException | LeroyException e) {
+        } catch (LeroyException e) {
             System.out.println(e.getMessage());
         }
 
         System.out.println(c1);
         System.out.println(c2);
     }
-
 }

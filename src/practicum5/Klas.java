0313e398acdb6c6da2eb258a3e70301734887629
LeroyAndrade <leroy_andrade@hotmail.com>
2024-11-30 21:35:16 +0100

    Opdracht voltooid

--- src/practicum5/Klas.java
+++ src/practicum5/Klas.java
@@ -6,7 +6,8 @@ public class Klas {
     private ArrayList<Leerling>deLeerlingen;
 
     public Klas(String kC) {
-        this.klasCode = kC;
+      this.klasCode = kC;
+      this.deLeerlingen = new ArrayList<>();
     }
 
     public void voegLeerlingToe(Leerling l){
@@ -14,15 +15,47 @@ public class Klas {
     }
 
     public void wijzigCijfer(String nm, double nweCijfer){
-        deLeerlingen.setCijfer(nweCijfer);
+       for (Leerling l : deLeerlingen){
+           if (l.getNaam().equals(nm)){
+               l.setCijfer(nweCijfer);
+           }
+       }
     }
 
-    public ArrayList<Leerling> getDeLeerlingen() {
-        return deLeerlingen;
+    public ArrayList<Leerling> getLeerlingen() {
+        return new ArrayList<>(deLeerlingen);
     }
 
     public int aantalLeerlingen(){
         return deLeerlingen.size();
     }
 
+    @Override
+    public String toString() {
+        String leerlingenInKlas = "In klas " + klasCode + " zitten de volgende leerlingen:\n";
+
+        for (Leerling l : deLeerlingen){
+            leerlingenInKlas += l.getNaam()
+                    + " heeft cijfer: " + l.getCijfer()+ "\n";
+        }
+
+        return leerlingenInKlas;
+    }
+
+    public static void main(String[] arg) {
+        Klas k = new Klas("V1Z");
+        Leerling l = new Leerling("Hans");
+        k.voegLeerlingToe(l);
+        l = new Leerling("Jan");
+        k.voegLeerlingToe(l);
+        l = new Leerling("Wim");
+        k.voegLeerlingToe(l);
+        System.out.println(k);
+        k.wijzigCijfer(new String("Hans"), 7.6);
+        k.wijzigCijfer("Klaas", 7.6);
+        System.out.println(k.toString());
+        System.out.println("Aantal leerlingen: " + k.aantalLeerlingen());
+    }
 }
+
+
